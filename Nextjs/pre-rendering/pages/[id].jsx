e1d456c4fe44f27b04e04f70a603e0ce70778918
Souvik Jana <souvikjana2013@gmail.com>
2023-01-15 00:13:23 +0530

    feat: add details page using ISR

--- Nextjs/pre-rendering/pages/[id].jsx
+++ Nextjs/pre-rendering/pages/[id].jsx
@@ -1,10 +1,23 @@
+import { useRouter } from "next/router";
+
+// SSG
+// export async function getStaticPaths() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   const posts = await response.json();
+
+//   const paths = posts.map((post) => ({ params: { id: `${post.id}` } }));
+
+//   return { paths, fallback: false };
+// }
+
+// ISR
 export async function getStaticPaths() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await response.json();
 
   const paths = posts.map((post) => ({ params: { id: `${post.id}` } }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
@@ -16,7 +29,42 @@ export async function getStaticProps({ params }) {
   return { props: { post } };
 }
 
+// SSG
+// const Post = ({ post }) => {
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         height: "100vh",
+//       }}
+//     >
+//       <div
+//         style={{
+//           border: "1px solid lightgray",
+//           height: "250px",
+//           width: "250px",
+//           padding: "0.5rem",
+//           margin: "0.5rem",
+//           cursor: "pointer",
+//         }}
+//         key={post.id}
+//       >
+//         {post.body}
+//       </div>
+//     </div>
+//   );
+// };
+
+// ISR
 const Post = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div
       style={{
--- Nextjs/pre-rendering/pages/index.jsx
+++ Nextjs/pre-rendering/pages/index.jsx
@@ -33,7 +33,7 @@ const Home = ({ posts }) => {
   );
 };
 
-//SSG
+// SSG
 // export async function getStaticProps() {
 //   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 //   const posts = await response.json();
@@ -41,12 +41,12 @@ const Home = ({ posts }) => {
 //   return { props: { posts: posts.slice(0, 15) } };
 // }
 
-//SSR
+// SSR
 // export async function getServerSideProps() {
 //   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
 //   const posts = await response.json();
 
-//   return { props: { posts } };
+//   return { props: { posts: posts.slice(0, 5) } };
 // }
 
 // ISR
